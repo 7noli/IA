0d874fae1d591df6ecf7cfa42d20d82f7c877bd0
riddhebakshi <riddheb@gmail.com>
2026-03-26 10:24:15 -0400

    for this one, i made input keys which come in the command line, "up", "down" "left" "right" are the main ones, and it will print in the cmmd line when you press a key, but its in the backend and does not show up YET on the gui

--- level.py
+++ level.py
@@ -20,7 +20,7 @@ class Level:
 
         self.display_surface.fill('black')
         self.all_sprites.draw(self.display_surface)
-        self.all_sprites.update()
+        self.all_sprites.update(dt)
 
 
 
